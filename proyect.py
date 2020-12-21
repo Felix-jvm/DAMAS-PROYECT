cc90037174c2003214504fd4e8ee2e2397824a29
FELIX <vasfelix020@gmail.com>
2020-12-21 08:57:40 -0400

    IT ADDS THE GRAPHIC USER INTERFACE

--- proyect.py
+++ proyect.py
@@ -1,1 +1,37 @@
-print('HI')
+import tkinter
+import os
+
+'''
+GRAPHIC USER INTERFACE
+'''
+##CREATES THE WINDOW
+window=tkinter.Frame(width=800,height=480)
+window.pack()
+
+##CREATES THE ORIGINLABEL
+originlabel=tkinter.Label(text='ORIGIN')
+originlabel.pack()
+originlabel.place(x=290,y=50)
+
+##CREATES THE DESTINATIONLABEL
+destinationlabel=tkinter.Label(text='TOWHERE')
+destinationlabel.pack()
+destinationlabel.place(x=270,y=80)
+
+##CREATES THE ORIGININPUT
+origininput=tkinter.Entry()
+origininput.pack()
+origininput.place(x=340,y=50)
+
+##CREATES THE DESTINATIONINPUT
+destinationinput=tkinter.Entry()
+destinationinput.pack()
+destinationinput.place(x=340,y=80)
+
+##CREATES THE CHECK BUTTON
+checkbuton=tkinter.Button(text='CHECK',command=lambda:check(origininput.get(),destinationinput.get()))
+checkbuton.pack()
+
+checkbuton.place(x=340,y=110)
+##CREATES THE CLEAN METHOD
+
